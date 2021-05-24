1bd61e10afe8c1b9e061934239eb33be8fefffb5
Brandon Hickey <bhickey94@gmail.com>
2021-05-23 20:12:39 -0700

    Update button.cpp
    Added support for momentary buttons.

--- button.cpp
+++ button.cpp
@@ -13,6 +13,10 @@ extern ConfigMap config;
 int last_button_state = 0;
 int press_count = 0;
 uint32_t last_button_press = 0;
+bool momentary_depressed = false;
+
+const bool momentary_button = true; 
+
 
 void setup_button()
 {
@@ -29,10 +33,14 @@ void loop_button()
   Check button state, debounce only allowing changes every 100ms
   This sketch is designed to use a latching button because that's
   what my Energizer tap lights came with. You'll need to modify
-  it if your buttons are momentary.
+  button_type to 1 if your buttons are momentary.
   */
   int button_state = digitalRead(INPUT_PIN);
-  if(button_state != last_button_state && millis() - last_button_press > 100) {
+
+  /*
+  Toggle Button press_count handler
+  */ 
+  if(!momentary_button && button_state != last_button_state && millis() - last_button_press > 100) {
     Serial.print("[BUTTON] ");
     Serial.print(millis() - last_button_press);
     last_button_press = millis();
@@ -43,6 +51,36 @@ void loop_button()
     Serial.print(button_state);
     Serial.print(" current count: ");
     Serial.println(press_count);
+    last_button_state = button_state;
+  }
+
+  /*
+  Momentary button press_count handler 
+  */
+  else if (momentary_button && !momentary_depressed && button_state != last_button_state && millis() - last_button_press > 100) {
+  Serial.print("[BUTTON] ");
+    Serial.print(" Button moved from ");
+    Serial.print(last_button_state);
+    Serial.print(" -> ");
+    Serial.print(button_state);
+    momentary_depressed = true;
+    last_button_state = button_state;
+  }
+  /*
+  Don't need to check if it's been 100 miliseconds, since momentary_depressed can't be true without checking this in the above conditional.
+  */
+  else if (momentary_button && momentary_depressed && button_state != last_button_state) {
+    Serial.print("[BUTTON] ");
+    Serial.print(millis() - last_button_press);
+    last_button_press = millis();
+    press_count += 1;
+    Serial.print(" Button moved from ");
+    Serial.print(last_button_state);
+    Serial.print(" -> ");
+    Serial.print(button_state);
+    Serial.print(" current count: ");
+    Serial.println(press_count);
+    momentary_depressed = false;
     last_button_state = button_state;
   }
 
